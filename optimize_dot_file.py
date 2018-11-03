1eb29d5435112eb694048d4db5578970f8b69d42
zk <send2zk@qq.com>
2018-11-03 16:18:41 +0800

    V1.0
    1/ name -> label
    2/ fix fill color ( node color “#ffc900”, edge “”)
    3/ fix no arrow

--- optimize_dot_file.py
+++ optimize_dot_file.py
@@ -5,6 +5,10 @@ import optparse
 # pip install python-igraph
 from igraph import *
 
+# Learn pyparsing !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+import os
+from pyparsing import Word, nums, Combine, alphas, Literal, ZeroOrMore, Group, Suppress
+
 # Usage message
 usgmsg = "Usage: delete_color.py file.gml"
 
@@ -36,42 +40,35 @@ def main():
     infos = f1.readlines()
     f1.seek(0, 0)
     node_flag = False
-    edge__flag = False
-    edge_graph__flag = False
+    edge_flag = False
+    edge_graph_flag = False
 
     for line in infos:
 
         line_new = line.replace("name", "label")
         node = re.match(".*node[^\dA-Za-z\u3007\u4E00-\u9FCB\uE815-\uE864]", line)
         edge = re.match('.*edge[^\dA-Za-z\u3007\u4E00-\u9FCB\uE815-\uE864]', line)
+        edge_graph = re.match('  ]', line)
 
         if node :
             node_flag = True
-            edge__flag = False
-            edge_graph__flag = False
+            edge_flag = False
+            edge_graph_flag = False
         if edge :
             node_flag = False
-            edge__flag = True
-            edge_graph__flag = False
+            edge_flag = True
+            edge_graph_flag = False
 
         if node_flag:
             # node color
             line_new = re.sub("fill \".*\"", "fill \"#ffc900\"", line_new)
-        if edge__flag:
+        if edge_flag:
             line_new = re.sub("fill \".*\"", "fill \"\"", line_new)
-            # edge graphics block:
-            edge_graph = re.match('.*graphics[^\dA-Za-z\u3007\u4E00-\u9FCB\uE815-\uE864]', line)
+            edge_graph = re.match('graphics \[', line)
             if edge_graph:
-                edge_graph__flag = True
-            if edge_graph__flag:
-                print "i m in graphics"
-
-                # fix no arrow
-                if re.match('.*targetArrow[^\dA-Za-z\u3007\u4E00-\u9FCB\uE815-\uE864]', line):
-                    edge_graph__flag = False
-                if re.match('.*]', line):
-                    line_new = "targetArrow \"standard\" ]\n"
-                    edge_graph__flag = False
+                edge_graph_flag = True
+            if edge_graph_flag:
+                line_new = "graphics \[ \n targetArrow \"standard\""
 
         f1.write(line_new)
     f1.close()
